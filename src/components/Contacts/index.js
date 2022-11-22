1fc36f7dc50e2372e50b895c63d32d6ab4b27c55
birsenbozkurt <birsenbozkurt4041@gmail.com>
2022-11-22 23:01:41 +0300

    filter

--- src/components/Contacts/index.js
+++ src/components/Contacts/index.js
@@ -4,7 +4,20 @@ import List from "./List";
 import Form from "./Form";
 
 function Contacts() {
-	const [contacts, setContacts] = useState([]);
+	const [contacts, setContacts] = useState([
+		{
+			fullname: "Birsen Bozkurt",
+			phone_number: "123123",
+		},
+		{
+			fullname: "Hilal Sari",
+			phone_number: "321321 ",
+		},
+		{
+			fullname: "Kübra Özkan",
+			phone_number: "456456",
+		},
+	]);
 
 	useEffect(() => {
 		console.log(contacts);
@@ -12,6 +25,7 @@ function Contacts() {
 	return (
 		<div>
 			<List contacts={contacts} />
+
 			<Form addContact={setContacts} contacts={contacts} />
 		</div>
 	);
